7aa7264198660b1ad65ce525cc637c1d9200bd3b
i3acsi <gasevskyv@gmail.com>
2021-04-21 15:41:59 +0700

    interfaces

---
+++ chapter001/interfaces/interface.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+type person struct {
+	name string
+	age  int
+}
+
+func (p person) NickName() string {
+	return p.name + " " + strconv.Itoa(p.age)
+}
+
+type NickNamed interface {
+	NickName() string
+}
+
+func joinChat(nn NickNamed) {
+	fmt.Println(nn.NickName() + " has joined.")
+}
+
+func interfaces() {
+	p := person{name: "Vasiliy", age: 34}
+	joinChat(p)
+
+}
+
+func main() {
+	interfaces()
+}
