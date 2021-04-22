85b3829dbec39d502a2c0e3d0d65a46d2e4944bb
i3acsi <gasevskyv@gmail.com>
2021-04-22 15:41:08 +0700

    handle get

--- chapter003/http-rest-api/go.mod
+++ chapter003/http-rest-api/go.mod
@@ -4,5 +4,6 @@ go 1.16
 
 require (
 	github.com/BurntSushi/toml v0.3.1
+	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/sirupsen/logrus v1.8.1
 )
--- chapter003/http-rest-api/internal/app/apiserver/apiserver.go
+++ chapter003/http-rest-api/internal/app/apiserver/apiserver.go
@@ -1,13 +1,17 @@
 package apiserver
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
+	"io"
+	"net/http"
 )
 
 // APIServer ...
 type APIServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 // New ...
@@ -15,6 +19,7 @@ func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -24,9 +29,11 @@ func (s *APIServer) Start() error {
 		return err
 	}
 	s.logger.Info("starting api server")
-	return nil
+	s.configureRouter()
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
+// configureLogger ...
 func (s *APIServer) configureLogger() error {
 	level, err := logrus.ParseLevel(s.config.LogLevel)
 	if err != nil {
@@ -38,4 +45,20 @@ func (s *APIServer) configureLogger() error {
 	})
 	s.logger.SetLevel(level)
 	return nil
+}
+
+// configureRouter ...
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello())
+
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	// define some args for this handler
+	// this code 'll done once, on init
+	// example: type req struct{ name string }
+
+	return func(writer http.ResponseWriter, request *http.Request) {
+		io.WriteString(writer, "Hello!")
+	}
 }
